4da2c5222c1462ae9ec40b36451f4958ad07ab04
ouail-nazim <ouailnazim20@gmail.com>
2021-10-27 21:06:14 +0100

    create send Api Get Request

--- app/src/main/java/com/example/learnapplication/SecondFragment.java
+++ app/src/main/java/com/example/learnapplication/SecondFragment.java
@@ -11,7 +11,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.learnapplication.databinding.FragmentSecondBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondFragment extends Fragment {
 
@@ -34,14 +49,48 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                sendGetRequest();
+//                NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
 
 
             }
         });
     }
-
+    // use Volley library to send api request
+    private void sendGetRequest() {
+        // url for the request
+        String url = "https://jsonplaceholder.typicode.com/todos";
+        // create RequestQueue instance
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        // create stringRequest instance
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            // get the response
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonarray = new JSONArray(response);
+                    for (int i = 0; i < jsonarray.length(); i++) {
+                        JSONObject jsonobject = jsonarray.getJSONObject(i);
+                        String id = jsonobject.getString("id");
+                        String title = jsonobject.getString("title");
+                        binding.textviewSecond.append(id + ":" + title + "\n");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    binding.textviewSecond.setText("Failed to Parse Json");
+                }
+            }
+        }, new Response.ErrorListener() {
+            // response fail
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                binding.textviewSecond.setText("Data : Response Failed");
+            }
+        });
+        // send the request
+        queue.add(stringRequest);
+    }
 
 
     @Override
