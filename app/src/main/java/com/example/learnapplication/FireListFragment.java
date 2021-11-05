03ed833fd2566fd620bc63ffdc1b3e671aa74ba5
ouail-nazim <ouailnazim20@gmail.com>
2021-11-05 20:53:34 +0100

    wrok

--- app/src/main/java/com/example/learnapplication/FireListFragment.java
+++ app/src/main/java/com/example/learnapplication/FireListFragment.java
@@ -2,63 +2,64 @@ package com.example.learnapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FireListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.learnapplication.databinding.FragmentFireListBinding;
+
 public class FireListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentFireListBinding binding;
+    ListView fireList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FireListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FireListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FireListFragment newInstance(String param1, String param2) {
-        FireListFragment fragment = new FireListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    String mTitle[] = {
+            "Bousbaa Moussa,Didouche Mourad,Constantine",
+            "UV15 nouvell ville,El khroub ,Constantine",
+            "Sidi Mabrouk,Constantine",
+    };
+    String mDescription[] = {
+            "By: @ouail_nazim (0540037061)",
+            "By: @kira_oussama (0554795175)",
+            "By: @lina_djaalb (07503461)",
+    };
+    int status[] = {1,2,3};
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentFireListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fireList = binding.fireList;
+
+        ListAdapter adapter = new ListAdapter(getContext(), mTitle,status, mDescription);
+        fireList.setAdapter(adapter);
+
+        fireList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), "Show Fire Num : "+ position+1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fire_list, container, false);
-    }
 }
